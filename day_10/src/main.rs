a09841e783947181725ea0f95482ce0bcb84a065
gbPagano <guilhermebpagano@gmail.com>
2023-12-13 11:51:39 -0300

    day 10

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,13 +1,14 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashSet;
 
 fn main() {
     let mut file = File::open("input.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
-    part_one(contents);
-    // part_two(contents);
+    // part_one(contents);
+    part_two(contents);
 }
 
 fn part_one(contents: String) {
@@ -20,7 +21,6 @@ fn part_one(contents: String) {
     let mut start_pos: (usize, usize) = (0, 0);
     let mut distance_grid = vec![vec![i64::MAX; grid[0].len()]; grid.len()];
     for (i, line) in grid.iter().enumerate() {
-        // println!("{:?}   {:?}", line, distance_grid[i]);
         for (j, tile) in line.iter().enumerate() {
             if tile == &'S' {
                 start_pos = (i, j);
@@ -30,13 +30,76 @@ fn part_one(contents: String) {
             }
         }
     }
+    let mut seen: HashSet<(usize, usize)> = HashSet::new();
+    seen.insert(start_pos);
 
-    evaluate_steps(&grid, &mut distance_grid, start_pos, 0);
-    // for (i, line) in grid.iter().enumerate() {
-    //     println!("{:?}   {:?}", line, distance_grid[i]);
-    // }
+    evaluate_steps(&grid, &mut distance_grid, start_pos, 0, &mut seen);
+
     let res = distance_grid.iter().flat_map(|row| row.iter()).filter(|&&x| x < i64::MAX).max().unwrap();
     println!("res: {}", res);
+}
+
+
+fn part_two(contents: String) {
+    let mut grid = contents
+        .split('\n')
+        .into_iter()
+        .map(|item| item.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let mut start_pos: (usize, usize) = (0, 0);
+    let mut distance_grid = vec![vec![i64::MAX; grid[0].len()]; grid.len()];
+    for (i, line) in grid.iter().enumerate() {
+        for (j, tile) in line.iter().enumerate() {
+            if tile == &'S' {
+                start_pos = (i, j);
+                distance_grid[i][j] = 0;
+            } else if tile == &'.' {
+                distance_grid[i][j] = 0;
+            }
+        }
+    }
+    let mut seen: HashSet<(usize, usize)> = HashSet::new();
+    seen.insert(start_pos);
+
+    evaluate_steps(&grid, &mut distance_grid, start_pos, 0, &mut seen);
+
+    
+    for (i, line) in grid.iter_mut().enumerate() {
+        for (j, tile) in line.iter_mut().enumerate() {
+            if !seen.contains(&(i, j)) {
+                *tile = '.';
+            }
+        }
+    }
+    
+    let mut res = 0;
+    
+    for (i, line) in grid.iter().enumerate() {
+        // println!("{:?}", line);
+        for (j, tile) in line.iter().enumerate() {
+            if tile == &'.' && it_is_outside(&grid, (i, j)) {
+                res += 1;
+            }
+        }
+    }
+    
+    println!("res: {}", res);
+}
+
+fn it_is_outside(grid: &Vec<Vec<char>>, current_pos: (usize, usize)) -> bool {
+    let (i, j) = current_pos;
+    let mut counts = 0;
+    for k in j..grid[i].len() {
+        // println!("{:?}", grid[i]);
+        let char = grid[i][k];
+        if ['|', 'J', 'L'].contains(&char) {
+            counts += 1;
+        }
+
+    }
+    // println!("{}", counts);
+    counts % 2 == 1
 }
 
 fn get_upper(
@@ -133,6 +196,7 @@ fn evaluate_steps(
     distance_grid: &mut Vec<Vec<i64>>,
     current_pos: (usize, usize),
     steps: i64,
+    seen: &mut HashSet<(usize, usize)>,
 ) {
     let (i, j) = current_pos;
     if distance_grid[i][j] > steps {
@@ -141,9 +205,7 @@ fn evaluate_steps(
 
     let mut next_positions: Vec<(usize, usize)> = Vec::new();
 
-    // pega a pos de cima
     let current = grid[i][j];
-    // println!("{current}");
     match current {
         'S' => {
             get_upper(grid, &distance_grid, current_pos, &mut next_positions, steps);
@@ -178,16 +240,8 @@ fn evaluate_steps(
         _ => (),
     }
 
-    // println!("{:?}", current_pos);
-    println!("{:?}", next_positions);
-    // for (i, line) in grid.iter().enumerate() {
-    //     println!("{:?}   {:?}", line, distance_grid[i]);
-    // }
-    // pega a pos de baixo
-    // pega a pos da direita
-    // pega a pos da esquerda
-
     for pos in next_positions {
-        evaluate_steps(grid, distance_grid, pos, steps + 1);
+        seen.insert(pos);
+        evaluate_steps(grid, distance_grid, pos, steps + 1, seen);
     }
 }
